a7cb5045caaf2e8a34bbbe10b1153e79f74615fa
Radosz <radosz.werner@gmail.com>
2024-04-22 17:11:19 +0200

    some data warning fixed

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import matplotlib.pyplot as plt
 
 # Load the data
 data = pd.read_csv('crypto-markets.csv')
-data['date'] = pd.to_datetime(data['date'])
-data = data.drop_duplicates(subset=['date'])
-data.set_index('date', inplace=True)
-missing_values = data.isnull().sum()
+btc_data = data[data['symbol'] == 'BTC'].copy()
+btc_data.loc[:, 'date'] = pd.to_datetime(btc_data['date'])
+btc_data.set_index('date', inplace=True)
+missing_values = btc_data.isnull().sum()
 print("Missing values:\n", missing_values)
 
 
 # Resample to daily frequency
-data_daily = data.resample('D').ffill()
+btc_data_daily = btc_data.resample('D').ffill()
+print(btc_data)
 
 # Plot the data
-data_daily['close'].plot(figsize=(12, 6), title='Bitcoin Closing Price')
+btc_data_daily['close'].plot(figsize=(12, 6), title='Bitcoin Closing Price')
 plt.show()
